a8ed5074dbffbb9ad2fc80675dd653d4c1baaa9e
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2023-08-22 14:00:21 +0200

    added endpoints

--- tno/mapeditor_dispatcher/apis/job.py
+++ tno/mapeditor_dispatcher/apis/job.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from typing import Optional, Dict
 
 from marshmallow_dataclass import dataclass
-from flask_smorest import Blueprint, abort
+from flask_smorest import Blueprint
 
 from flask.views import MethodView
 
@@ -48,26 +48,44 @@ class JobInput:
 
 
 @dataclass
-class JobPostResponse:
+class JobStatusResponse:
     job_id: uuid.UUID
-    status: str
+    status: JobStatus
 
 
 @dataclass
-class JobDetails:
+class JobResultResponse:
+    job_id: uuid.UUID
+    output_esdl: Optional[str]
+
+
+@dataclass
+class JobDeleteResponse:
+    job_id: uuid.UUID
+    deleted: bool
+
+
+@dataclass
+class JobLogsResponse:
+    job_id: uuid.UUID
+    logs: Optional[str]
+
+
+@dataclass
+class JobDetailsResponse:
     job_id: uuid.UUID
     job_name: str
     work_flow_type: str
     user_name: str
     project_name: str
-    status: str
+    status: JobStatus
     input_config: str
     input_esdl: str
     output_esdl: Optional[str]
     added_at: datetime
     running_at: Optional[datetime]
     stopped_at: Optional[datetime]
-    log: Optional[str]
+    logs: Optional[str]
 
 
 @dataclass
@@ -76,27 +94,15 @@ class JobSummary:
     job_name: str
     user_name: str
     project_name: str
-    status: str
+    status: JobStatus
     added_at: datetime
-
-
-#
-# @dataclass
-# class LogResponse:
-#     job_id: int
-#     log: str
-#
-#
-# @dataclass
-# class ESDLResultResponse:
-#     job_id: int
-#     esdl: str
+    stopped_at: Optional[datetime]
 
 
 @api.route("/")
 class JobAPI(MethodView):
     @api.arguments(JobInput.Schema())
-    @api.response(200, JobPostResponse.Schema())
+    @api.response(200, JobStatusResponse.Schema())
     def post(self, request: JobInput):
         """
         Start new job
@@ -108,7 +114,7 @@ class JobAPI(MethodView):
             new_job = NwnJob(job_id=job_id, user_name=request.user_name, project_name=request.project_name)
             session.add(new_job)
 
-        return JobPostResponse(job_id=job_id, status=JobStatus.REGISTERED)
+        return JobStatusResponse(job_id=job_id, status=JobStatus.REGISTERED)
 
     @api.response(200, JobSummary.Schema(many=True))
     def get(self):
@@ -119,17 +125,54 @@ class JobAPI(MethodView):
 
 
 @api.route("/<string:job_id>")
-class JobDetails(MethodView):
-    @api.response(200, JobDetails.Schema())
+class JobFromIdAPI(MethodView):
+    @api.response(200, JobDetailsResponse.Schema())
     def get(self, job_id: int):
         """
         Return job details
         """
         return nwn_client.get_job_details(job_id)
 
+    @api.response(200, JobDeleteResponse.Schema())
+    def delete(self, job_id: int):
+        """
+        Delete job
+        """
+        return JobDeleteResponse(job_id=job_id, deleted=nwn_client.delete_job(job_id))
+
+
+@api.route("/<string:job_id>/status")
+class JobStatusAPI(MethodView):
+    @api.response(200, JobStatusResponse.Schema())
+    def get(self, job_id: int):
+        """
+        Return job status
+        """
+        return JobStatusResponse(job_id=job_id, status=nwn_client.get_job_status(job_id))
+
+
+@api.route("/<string:job_id>/result")
+class JobResultAPI(MethodView):
+    @api.response(200, JobResultResponse.Schema())
+    def get(self, job_id: int):
+        """
+        Return job result
+        """
+        return JobResultResponse(job_id=job_id, output_esdl=nwn_client.get_job_output_esdl(job_id))
+
+
+@api.route("/<string:job_id>/logs")
+class JobLogsAPI(MethodView):
+    @api.response(200, JobLogsResponse.Schema())
+    def get(self, job_id: int):
+        """
+        Return job logs
+        """
+        return JobLogsResponse(job_id=job_id, logs=nwn_client.get_job_logs(job_id))
+
 
 @api.route("/user/<string:user_name>")
-class JobByUser(MethodView):
+class JobsByUserAPI(MethodView):
     @api.response(200, JobSummary.Schema(many=True))
     def get(self, user_name: str):
         """
@@ -139,49 +182,10 @@ class JobByUser(MethodView):
 
 
 @api.route("/project/<string:project_name>")
-class JobByProject(MethodView):
+class JobByProjectAPI(MethodView):
     @api.response(200, JobSummary.Schema(many=True))
     def get(self, project_name: str):
         """
         Return all jobs from project
         """
         return nwn_client.get_jobs_from_project(project_name)
-
-
-# @api.route("/job/<int:id>")
-# class GetJob(MethodView):
-#     """
-#     Returns a specific job
-#     """
-#
-#     @api.response(200, JobDetails.Schema(many=False))
-#     def get(self, job_id: int):
-#         print(job_id)
-#         return JobDetails(username="Ewoud", id=job_id, name="First run", status="FINISHED", timestamp=datetime.now())
-#
-#
-# @api.route("/job/log/<int:id>")
-# class GetLog(MethodView):
-#     """
-#     Returns the logging of the workflow for a specific Job
-#     """
-#
-#     @api.response(200, LogResponse.Schema())
-#     def get(self, job_id: int):
-#         print(job_id)
-#         return LogResponse(job_id=job_id, log="This is a log with no errors")
-#
-#
-# @api.route("/job/result/<int:id>")
-# class GetESDLResult(MethodView):
-#     """
-#     Returns the ESDL result of a specific job, if available
-#     """
-#
-#     @api.response(200, ESDLResultResponse.Schema())
-#     def get(self, job_id: int):
-#         print(job_id)
-#         if None:  # check if result is available first
-#             abort(404, message="Job is not finished. Current state is: Running")
-#         # b64 encode esdl string
-#         return ESDLResultResponse(job_id=job_id, esdl="<EnergySystem/>")
