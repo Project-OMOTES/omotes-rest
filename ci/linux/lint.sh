55311c534c79beaf18f54acd2f8b39cc4ed0bd60
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-03-22 21:49:09 +0100

    fix lint and typecheck actions

--- ci/linux/lint.sh
+++ ci/linux/lint.sh
@@ -4,4 +4,4 @@ if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   echo "Activating .venv first."
   . .venv/bin/activate
 fi
-flake8 ./src/omotes_orchestrator ./unit_test/
+flake8 ./src/omotes_rest ./unit_test/
--- ci/linux/typecheck.sh
+++ ci/linux/typecheck.sh
@@ -4,4 +4,4 @@ if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   echo "Activating .venv first."
   . .venv/bin/activate
 fi
-python -m mypy ./src/omotes_orchestrator ./unit_test/
+python -m mypy ./src/omotes_rest ./unit_test/
