280bbe153c0bea3409bdc45a69ffd4fe88f73b0f
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-04-22 13:42:41 +0200

    14: Remove unused import.

--- src/omotes_rest/db_models/job_rest.py
+++ src/omotes_rest/db_models/job_rest.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 import sqlalchemy as db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 
