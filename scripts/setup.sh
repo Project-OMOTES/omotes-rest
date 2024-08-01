98482e6b041352627c2b57a08dc532fab5eb560f
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-08-01 14:58:34 +0200

    clean up docker compose files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,18 +6,18 @@
 #$DOCKER_COMPOSE stop
 
 # Deploy postgres omotes_rest schema
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml --profile=manual_dev down rest_postgres_db_dev
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml up -d --wait rest_postgres_db
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d postgres -c 'CREATE DATABASE omotes_rest;'
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "CREATE USER ${POSTGRES_USERNAME} WITH PASSWORD '${POSTGRES_PASSWORD}';"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER USER ${POSTGRES_USERNAME} WITH PASSWORD '${POSTGRES_PASSWORD}';"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON DATABASE omotes_rest TO ${POSTGRES_USERNAME};"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON SCHEMA public TO ${POSTGRES_USERNAME};"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${POSTGRES_USERNAME};"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ${POSTGRES_USERNAME};"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ${POSTGRES_USERNAME};"
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml --profile=manual_dev down rest_postgres_db_dev
+$DOCKER_COMPOSE -f ./docker-compose.yml up -d --wait rest_postgres_db
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d postgres -c 'CREATE DATABASE omotes_rest;'
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "CREATE USER ${POSTGRES_USERNAME} WITH PASSWORD '${POSTGRES_PASSWORD}';"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER USER ${POSTGRES_USERNAME} WITH PASSWORD '${POSTGRES_PASSWORD}';"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON DATABASE omotes_rest TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON SCHEMA public TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO ${POSTGRES_USERNAME};"
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO ${POSTGRES_USERNAME};"
 
 # Upgrade omotes_rest tables
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml build rest_postgres_db_upgrade
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml run --rm rest_postgres_db_upgrade
+$DOCKER_COMPOSE -f ./docker-compose.yml build rest_postgres_db_upgrade
+$DOCKER_COMPOSE -f ./docker-compose.yml run --rm rest_postgres_db_upgrade
--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,6 +2,6 @@
 
 . scripts/_select_docker_compose.sh
 
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml --profile=manual_dev --profile=manual_setup down
-$DOCKER_COMPOSE -f ./docker-compose.yml down
-$DOCKER_COMPOSE -f ./docker-compose.yml -f ./docker-compose.dev.yml --profile=manual_dev up --wait --build
+$DOCKER_COMPOSE -f ./docker-compose.dev.yml -f ./docker-compose.override.dev.yml --profile=manual_dev --profile=manual_setup down
+$DOCKER_COMPOSE -f ./docker-compose.yml -f ./docker-compose.override.dev.yml down
+$DOCKER_COMPOSE -f ./docker-compose.yml -f ./docker-compose.override.dev.yml up --wait --build
