2759c7cdb6f58eee563e873529d36bad00933e64
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-09-13 15:23:27 +0200

    cleaned up scripts after docker compose updates

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,5 @@
 
 . scripts/_select_docker_compose.sh
 
-$DOCKER_COMPOSE -f ./docker-compose.dev.yml --profile=manual_dev --profile=manual_setup down
-$DOCKER_COMPOSE -f ./docker-compose.yml down
+$DOCKER_COMPOSE -f ./docker-compose.yml --profile=manual_setup down
 $DOCKER_COMPOSE -f ./docker-compose.yml up --wait
