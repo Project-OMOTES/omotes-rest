45da187111f730b7dffbbb6ca35eb3cf80d01e8b
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-09-19 16:03:25 +0200

    30: Also update all dependencies on update_dependencies.sh.

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -4,5 +4,5 @@ if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   . .venv/bin/activate
 fi
 
-pip-compile --output-file=requirements.txt pyproject.toml
-pip-compile --extra=dev -c requirements.txt --output-file=dev-requirements.txt pyproject.toml
+pip-compile -U --output-file=requirements.txt pyproject.toml
+pip-compile -U --extra=dev -c requirements.txt --output-file=dev-requirements.txt pyproject.toml
