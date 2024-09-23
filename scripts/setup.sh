ec4c93ad3c5811bcd8c66f216abf5e4d07fedd78
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-09-23 21:04:02 +0200

    fix setup script and docker compose port

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,4 @@
 $DOCKER_COMPOSE -f ./docker-compose.yml down rest_postgres_db
 $DOCKER_COMPOSE -f ./docker-compose.yml up -d --wait rest_postgres_db
 
-$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -v PG_USERNAME=POSTGRES_USERNAME -v PG_PASSWORD=POSTGRES_PASSWORD -v PG_DB=omotes_rest -f /setup/init.sql
+$DOCKER_COMPOSE -f ./docker-compose.yml exec rest_postgres_db psql -d omotes_rest -v PG_USERNAME=$POSTGRES_USERNAME -v PG_PASSWORD=$POSTGRES_PASSWORD -v PG_DB=omotes_rest -f /setup/init.sql
